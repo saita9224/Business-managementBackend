46a9f49d660f2d2f715042aeba83f45b8ac7ce71
saita9224 <saitacollins@gmail.com>
2026-07-14 17:06:10 +0800

    modifiried static file

--- backend/settings.py
+++ backend/settings.py
@@ -164,7 +164,14 @@ USE_TZ        = True
 # ======================================================
 # STATIC FILES
 # ======================================================
-STATIC_URL = 'static/'
+STATIC_URL  = 'static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+
+# ======================================================
+# MEDIA FILES (user uploads)
+# ======================================================
+MEDIA_URL  = 'media/'
+MEDIA_ROOT = BASE_DIR / 'media'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
